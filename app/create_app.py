6ffa58553338208e6474e2157626108cada36d33
maciejpajak <maciej.pajak@student.uj.edu.pl>
2023-05-13 19:54:10 +0200

    gunicorn

--- app/create_app.py
+++ app/create_app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+
 def create_app():
     app = Flask(__name__)
+
     @app.route('/')
     def index():
         return render_template('index.html')
+
     @app.route('/devops-project')
     def about():
         return render_template('devops-project.html')
+
     return app
